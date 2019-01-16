a77cd868aeb9e19a1883ad5b3725291d821d7037
unknown <gomezger.a9@gmail.com>
2019-01-16 11:36:22 -0300

    added .env file, module of form and the get request for the email sending

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ app.listen(8080, () => {
     console.log('Server is running in port 8080');
 })
 
-app.get('/myaction', (req,res) => {
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/emailForm.html');
+})
+
+app.get('/emailForm', (req,res) => {
+    res.sendFile(__dirname + '/emailForm.html');
+    const email = req.query.email;
+    const text = req.query.text;
+    console.log("[" + email, text + "]");
+    const sgMail = require('@sendgrid/mail');
+    const msg = {
+        to: email,
+        from: 'test@example.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: text,
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    };
+sgMail.send(msg);
     
 })
